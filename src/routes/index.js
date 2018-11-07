9b9ed010ca5e73118a15123fca2c41693538c010
Kai Davenport <kaiyadavenport@gmail.com>
2018-11-07 10:26:06 +0000

    #14: protect api routes with user handlers - add user has a special case where if the user count is zero - a user can be added by a non-logged in user

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,38 @@ const settings = require('../settings')
 
 const basePath = (path) => `${settings.baseUrl}${path}`
 
+const requireUser = (type) => (req, res, next) => {
+  if(!req.user) {
+    res
+      .status(403)
+      .json({
+        error: 'user account required'
+      })
+    return
+  }
+  if(type && req.user.type != type) {
+    res
+      .status(403)
+      .json({
+        error: `user account of type ${type} required`
+      })
+    return
+  }
+  next()
+}
+
+// if there are no users - then allow a non-logged in user
+// to create a new user - otherwise they must be an admin
+const addUserAuthHandler = (userBackend) => (req, res, next) => {
+  const adminHandler = requireUser('admin')
+
+  userBackend.count({}, (err, userCount) => {
+    if(err) return next(err)
+    if(userCount <= 0) return next()
+    adminHandler(req, res, next)
+  })
+}
+
 const Routes = (app, backends) => {
 
   const config = ConfigRoutes(backends)
@@ -15,24 +47,24 @@ const Routes = (app, backends) => {
   app.get(basePath('/config/values'), config.values)
   app.get(basePath('/config/aws'), config.aws)
 
-  app.get(basePath('/user'), user.list)
+  app.get(basePath('/user'), requireUser('admin'), user.list)
   app.get(basePath('/user/status'), user.status)
   app.post(basePath('/user/login'), user.login)
-  app.get(basePath('/user/logout'), user.logout)
-  app.post(basePath('/user'), user.create)
+  app.get(basePath('/user/logout'), requireUser(), user.logout)
+  app.post(basePath('/user'), addUserAuthHandler(backends.user), user.create)
 
-  app.get(basePath('/cluster'), cluster.list)
-  app.get(basePath('/cluster/:id'), cluster.get)
-  app.post(basePath('/cluster'), cluster.create)
-  app.delete(basePath('/cluster/:id'), cluster.destroy)
-  app.put(basePath('/cluster/cleanup/:id'), cluster.cleanup)
-  app.put(basePath('/cluster/undeploy/:id'), cluster.undeploy)
-  app.get(basePath('/cluster/status/:id'), cluster.status)
-  app.get(basePath('/cluster/info/:id'), cluster.info)
-  app.get(basePath('/cluster/kubeconfig/:id'), cluster.kubeconfig)
-  app.get(basePath('/cluster/kopsconfig/:id'), cluster.kopsconfig)
-  app.post(basePath('/cluster/keypair/create'), cluster.createKeypair)
-  app.post(basePath('/cluster/deploy/:id'), cluster.deploy)
+  app.get(basePath('/cluster'), requireUser(), cluster.list)
+  app.get(basePath('/cluster/:id'), requireUser(), cluster.get)
+  app.post(basePath('/cluster'), requireUser(), cluster.create)
+  app.delete(basePath('/cluster/:id'), requireUser(), cluster.destroy)
+  app.put(basePath('/cluster/cleanup/:id'), requireUser(), cluster.cleanup)
+  app.put(basePath('/cluster/undeploy/:id'), requireUser(), cluster.undeploy)
+  app.get(basePath('/cluster/status/:id'), requireUser(), cluster.status)
+  app.get(basePath('/cluster/info/:id'), requireUser(), cluster.info)
+  app.get(basePath('/cluster/kubeconfig/:id'), requireUser(), cluster.kubeconfig)
+  app.get(basePath('/cluster/kopsconfig/:id'), requireUser(), cluster.kopsconfig)
+  app.post(basePath('/cluster/keypair/create'), requireUser(), cluster.createKeypair)
+  app.post(basePath('/cluster/deploy/:id'), requireUser(), cluster.deploy)
 }
 
 module.exports = Routes
