392b5fdc7ecade1af1b3f768a14ed0308886b977
Kai Davenport <kaiyadavenport@gmail.com>
2019-04-23 14:45:48 +0100

    wrap the test for cluster list with all users in a loop

--- test/router/cluster.test.js
+++ test/router/cluster.test.js
@@ -24,28 +24,31 @@ app.testSuiteWithApp(({
     })
   })
 
-  tape('cluster routes -> list clusters as superuser', (t) => {
-    userUtils.withUser({
-      url,
-      t,
-      user: userUtils.USERS.superuser,
-    }, 
-    (next) => {
-      tools.sessionRequest({
-        method: 'get',
-        url: `${url}/clusters`,
-        json: true,
-      }, (err, res, body) => {
-        t.notok(err, `there is no error`)
-        t.equal(res.statusCode, 200, `200 code`)
-        t.deepEqual(body, [], `the body is an empty list of clusters`)
-        next()
-      })
-    },
-    (err) => {
+  tape('cluster routes -> list clusters as all users', (t) => {
+
+    async.eachSeries(Object.keys(userUtils.USERS), (userKey, nextUser) => {
+      userUtils.withUser({
+        url,
+        t,
+        user: userUtils.USERS[userKey],
+      }, 
+      (next) => {
+        tools.sessionRequest({
+          method: 'get',
+          url: `${url}/clusters`,
+          json: true,
+        }, (err, res, body) => {
+          t.notok(err, `there is no error`)
+          t.equal(res.statusCode, 200, `200 code`)
+          t.deepEqual(body, [], `the body is an empty list of clusters`)
+          next()
+        })
+      }, nextUser)
+    }, (err) => {
       t.notok(err, `there was no error`)
       t.end()
     })
   })
+
 
 })
