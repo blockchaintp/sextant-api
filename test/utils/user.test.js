e8f36147ef04e32e824cc0a8f84594ccdbb31c81
Kai Davenport <kaiyadavenport@gmail.com>
2019-05-03 15:28:27 +0100

    defend errors in user utils test

--- test/utils/user.test.js
+++ test/utils/user.test.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const tape = require('tape')
-const randomstring = require('randomstring')
 const async = require('async')
 const userUtils = require('../../src/utils/user')
+const tools = require('../tools')
 
 tape('user utils -> get and compare hash (correct)', (t) => {
 
@@ -12,11 +12,10 @@ tape('user utils -> get and compare hash (correct)', (t) => {
   async.waterfall([
     (next) => userUtils.getPasswordHash(PASSWORD, next),
     (hash, next) => userUtils.compareHashedPasswords(PASSWORD, hash, next),
-  ], (err, result) => {
-    t.notok(err, `there was no error`)
+  ], tools.errorWrapper(t, (result) => {
     t.ok(result, `the comparison was correct`)
     t.end()
-  })
+  }))
 
 })
 
@@ -27,11 +26,10 @@ tape('user utils -> get and compare hash (incorrect)', (t) => {
   async.waterfall([
     (next) => userUtils.getPasswordHash(PASSWORD, next),
     (hash, next) => userUtils.compareHashedPasswords('oranges', hash, next),
-  ], (err, result) => {
-    t.notok(err, `there was no error`)
+  ], tools.errorWrapper(t, (result) => {
     t.notok(result, `the comparison was not correct`)
     t.end()
-  })
+  }))
 
 })
 
@@ -44,14 +42,12 @@ tape('user utils -> get and compare token (correct)', (t) => {
   async.waterfall([
     (next) => userUtils.getToken(ID, KEY, SECRET, next),
     (token, next) => userUtils.decodeToken(token, SECRET, next),
-  ], (err, decoded) => {
-    t.notok(err, `there was no error`)
+  ], tools.errorWrapper(t, (decoded) => {
     t.equal(decoded.id, ID, `the username was correct`)
     t.ok(decoded.server_side_key, `there is a server_side_key`)
     t.equal(decoded.server_side_key, KEY, `the server_side_key was correct`)
     t.end()
-  })
-
+  }))
 })
 
 tape('user utils -> get and compare token no secret', (t) => {
@@ -64,7 +60,7 @@ tape('user utils -> get and compare token no secret', (t) => {
     (next) => userUtils.getToken(ID, KEY, SECRET, next),
     (token, next) => userUtils.decodeToken(token, SECRET, next),
   ], (err, decoded) => {
-    t.ok(err, `there was no error`)
+    t.ok(err, `there was an error`)
     t.equal(decoded, undefined, `there was no data`)
     t.end()
   })
@@ -81,7 +77,7 @@ tape('user utils -> get and compare token (incorrect secret)', (t) => {
     (next) => userUtils.getToken(ID, KEY, SECRET, next),
     (token, next) => userUtils.decodeToken(token, SECRET + 'bad', next),
   ], (err, decoded) => {
-    t.ok(err, `there was no error`)
+    t.ok(err, `there was an error`)
     t.equal(decoded, undefined, `there was no data`)
     t.end()
   })
@@ -98,7 +94,7 @@ tape('user utils -> get and compare token (incorrect token)', (t) => {
     (next) => userUtils.getToken(ID, KEY, SECRET, next),
     (token, next) => userUtils.decodeToken(token + 'bad', SECRET, next),
   ], (err, decoded) => {
-    t.ok(err, `there was no error`)
+    t.ok(err, `there was an error`)
     t.equal(decoded, undefined, `there was no data`)
     t.end()
   })
@@ -111,13 +107,10 @@ tape('user utils -> check tokens are generated the same way twice', (t) => {
   const SECRET = 'oranges'
   const KEY = userUtils.getTokenServerSideKey()
 
-  userUtils.getToken(ID, KEY, SECRET, (err, firstToken) => {
-    t.notok(err, `there was no initial error`)
-
-    userUtils.getToken(ID, KEY, SECRET, (err, secondToken) => {
-      t.notok(err, `there was no initial error`)
+  userUtils.getToken(ID, KEY, SECRET, tools.errorWrapper(t, (firstToken) => {
+    userUtils.getToken(ID, KEY, SECRET, tools.errorWrapper(t, (secondToken) => {
       t.equal(firstToken, secondToken, `the two tokens are the same`)
       t.end()
-    })
-  })
+    }))
+  }))
 })
