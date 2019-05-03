51c47e70c63f5eaca3212f428028500c12ac909b
Kai Davenport <kaiyadavenport@gmail.com>
2019-05-03 18:33:38 +0100

    test for updating a cluster copies the desired_state to applied state but keeps the old pointers to secrets

--- test/task_handlers/cluster.test.js
+++ test/task_handlers/cluster.test.js
@@ -89,5 +89,55 @@ database.testSuiteWithDatabase(getConnection => {
 
   })
 
+  tape('cluster controller -> update cluster', (t) => {
+  
+    const controller = getController()
+    const taskProcessor = getTaskProcessor({})
+    const testUser = userMap[PERMISSION_USER.admin]
+    const clusterData = fixtures.SIMPLE_CLUSTER_DATA[1]
+
+    const NEW_NAME = 'new name'
+    const NEW_API_SERVER = 'http://otherapi.com'
+
+    async.series([
+
+      next => taskProcessor.start(next),
+
+      next => controller.update({
+        user: testUser,
+        id: testClusters.admin.id,
+        data: {
+          name: NEW_NAME,
+          desired_state: {
+            apiServer: NEW_API_SERVER,
+          }
+        },
+      }, next),
+
+      next => setTimeout(next, TASK_CONTROLLER_LOOP_DELAY * 2),
+
+      next => controller.get({
+        id: testClusters.admin.id,
+      }, (err, cluster) => {
+        if(err) return next(err)
+
+        t.deepEqual(cluster.desired_state, cluster.applied_state, `the applied_state has been updated to the desired_state`)
+
+        t.equal(cluster.name, NEW_NAME, `the name has been updated`)
+        t.equal(cluster.desired_state.apiServer, NEW_API_SERVER, `the apiServer has been updated`)
+        t.equal(cluster.desired_state.ca_id, testClusters.admin.desired_state.ca_id, `the ca is the same as before`)
+        t.equal(cluster.desired_state.token_id, testClusters.admin.desired_state.token_id, `the token is the same as before`)
+
+        next()
+      }),
+    ], (err) => {
+      t.notok(err, `there was no error`)
+      taskProcessor.stop(() => {
+        t.end()
+      })
+    })
+
+  })
+
   
 })
