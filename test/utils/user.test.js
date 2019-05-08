e4fd867d3b57eed8986c6b16fccbbe207aaa9e20
Kai Davenport <kaiyadavenport@gmail.com>
2019-05-08 12:05:47 +0100

    passing tests for user utils as async functions

--- test/utils/user.test.js
+++ test/utils/user.test.js
@@ -4,113 +4,103 @@ const tape = require('tape')
 const async = require('async')
 const userUtils = require('../../src/utils/user')
 const tools = require('../tools')
+const asyncTest = require('../asyncTest')
 
-tape('user utils -> get and compare hash (correct)', (t) => {
-
+asyncTest('user utils -> get and compare hash (correct)', async (t) => {
   const PASSWORD = 'apples'
-
-  async.waterfall([
-    (next) => userUtils.getPasswordHash(PASSWORD, next),
-    (hash, next) => userUtils.compareHashedPasswords(PASSWORD, hash, next),
-  ], tools.errorWrapper(t, (result) => {
-    t.ok(result, `the comparison was correct`)
-    t.end()
-  }))
-
+  const hash = await userUtils.getPasswordHash(PASSWORD)
+  const result = await userUtils.compareHashedPasswords(PASSWORD, hash)
+  t.ok(result, `the comparison was correct`)
 })
 
-tape('user utils -> get and compare hash (incorrect)', (t) => {
-
+asyncTest('user utils -> get and compare hash (incorrect)', async (t) => {
   const PASSWORD = 'apples'
-
-  async.waterfall([
-    (next) => userUtils.getPasswordHash(PASSWORD, next),
-    (hash, next) => userUtils.compareHashedPasswords('oranges', hash, next),
-  ], tools.errorWrapper(t, (result) => {
-    t.notok(result, `the comparison was not correct`)
-    t.end()
-  }))
-
+  const hash = await userUtils.getPasswordHash(PASSWORD)
+  const result = await userUtils.compareHashedPasswords('oranges', hash)
+  t.notok(result, `the comparison was not correct`)
 })
 
-tape('user utils -> get and compare token (correct)', (t) => {
+asyncTest('user utils -> get and compare token (correct)', async (t) => {
 
   const ID = 10
   const SECRET = 'oranges'
   const KEY = userUtils.getTokenServerSideKey()
 
-  async.waterfall([
-    (next) => userUtils.getToken(ID, KEY, SECRET, next),
-    (token, next) => userUtils.decodeToken(token, SECRET, next),
-  ], tools.errorWrapper(t, (decoded) => {
-    t.equal(decoded.id, ID, `the username was correct`)
-    t.ok(decoded.server_side_key, `there is a server_side_key`)
-    t.equal(decoded.server_side_key, KEY, `the server_side_key was correct`)
-    t.end()
-  }))
+  const token = await userUtils.getToken(ID, KEY, SECRET)
+  const decoded = await userUtils.decodeToken(token, SECRET)
+
+  t.equal(decoded.id, ID, `the username was correct`)
+  t.ok(decoded.server_side_key, `there is a server_side_key`)
+  t.equal(decoded.server_side_key, KEY, `the server_side_key was correct`)
 })
 
-tape('user utils -> get and compare token no secret', (t) => {
+asyncTest('user utils -> get and compare token no secret', async (t) => {
 
   const ID = 10
   const SECRET = ''
   const KEY = userUtils.getTokenServerSideKey()
 
-  async.waterfall([
-    (next) => userUtils.getToken(ID, KEY, SECRET, next),
-    (token, next) => userUtils.decodeToken(token, SECRET, next),
-  ], (err, decoded) => {
-    t.ok(err, `there was an error`)
-    t.equal(decoded, undefined, `there was no data`)
-    t.end()
-  })
+  let error = null
+  let decoded = null
 
+  try {
+    const token = await userUtils.getToken(ID, KEY, SECRET)
+    decoded = await userUtils.decodeToken(token, SECRET)
+  } catch(err) {
+    error = err
+  }
+
+  t.ok(error, `there was an error`)
+  t.equal(decoded, null, `there was no data`)
 })
 
-tape('user utils -> get and compare token (incorrect secret)', (t) => {
+asyncTest('user utils -> get and compare token (incorrect secret)', async (t) => {
 
   const ID = 10
   const SECRET = 'oranges'
   const KEY = userUtils.getTokenServerSideKey()
 
-  async.waterfall([
-    (next) => userUtils.getToken(ID, KEY, SECRET, next),
-    (token, next) => userUtils.decodeToken(token, SECRET + 'bad', next),
-  ], (err, decoded) => {
-    t.ok(err, `there was an error`)
-    t.equal(decoded, undefined, `there was no data`)
-    t.end()
-  })
+  let error = null
+  let decoded = null
 
+  try {
+    const token = await userUtils.getToken(ID, KEY, SECRET)
+    decoded = await userUtils.decodeToken(token, SECRET + 'bad')
+  } catch(err) {
+    error = err
+  }
+
+  t.ok(error, `there was an error`)
+  t.equal(decoded, null, `there was no data`)
 })
 
-tape('user utils -> get and compare token (incorrect token)', (t) => {
+asyncTest('user utils -> get and compare token (incorrect token)', async (t) => {
 
   const ID = 10
   const SECRET = 'oranges'
   const KEY = userUtils.getTokenServerSideKey()
 
-  async.waterfall([
-    (next) => userUtils.getToken(ID, KEY, SECRET, next),
-    (token, next) => userUtils.decodeToken(token + 'bad', SECRET, next),
-  ], (err, decoded) => {
-    t.ok(err, `there was an error`)
-    t.equal(decoded, undefined, `there was no data`)
-    t.end()
-  })
+  let error = null
+  let decoded = null
 
+  try {
+    const token = await userUtils.getToken(ID, KEY, SECRET)
+    decoded = await userUtils.decodeToken(token + 'bad', SECRET)
+  } catch(err) {
+    error = err
+  }
+
+  t.ok(error, `there was an error`)
+  t.equal(decoded, null, `there was no data`)
 })
 
-tape('user utils -> check tokens are generated the same way twice', (t) => {
+asyncTest('user utils -> check tokens are generated the same way twice', async (t) => {
 
   const ID = 10
   const SECRET = 'oranges'
   const KEY = userUtils.getTokenServerSideKey()
 
-  userUtils.getToken(ID, KEY, SECRET, tools.errorWrapper(t, (firstToken) => {
-    userUtils.getToken(ID, KEY, SECRET, tools.errorWrapper(t, (secondToken) => {
-      t.equal(firstToken, secondToken, `the two tokens are the same`)
-      t.end()
-    }))
-  }))
+  const firstToken = await userUtils.getToken(ID, KEY, SECRET)
+  const secondToken = await userUtils.getToken(ID, KEY, SECRET)
+  t.equal(firstToken, secondToken, `the two tokens are the same`)
 })
