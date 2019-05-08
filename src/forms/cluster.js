82a60128773fbd26ad92de6ee776b70e416fdaff
Kai Davenport <kaiyadavenport@gmail.com>
2019-05-08 10:00:50 +0100

    use a regular expression for api server that allows non TLD hostnames

--- src/forms/cluster.js
+++ src/forms/cluster.js
@@ -1,7 +1,11 @@
 const builder = require('./builder')
 
 const validators = {
-  url: ['url', 'Must be a valid url - e.g. http://apiserver.com'],
+  url: [
+    'matches', 
+    '^(?:([a-z0-9+.-]+):\\/\\/)(?:\\S+(?::\\S*)?@)?(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*\\.?)(?::\\d{2,5})?(?:[/?#]\\S*)?$',
+    'Must be a valid url - e.g. http://apiserver.com'
+  ],
 }
 
 const fields = {
