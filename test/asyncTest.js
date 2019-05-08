30bdd5808ec3fb73702a4e8226d225b6ffdc3b48
Kai Davenport <kaiyadavenport@gmail.com>
2019-05-08 17:27:18 +0100

    add cleanup step to async test util

--- test/asyncTest.js
+++ test/asyncTest.js
@@ -1,12 +1,15 @@
 const tape = require('tape')
 
-const asyncTest = (name, handler) => {
+const asyncTest = (name, handler, cleanup) => {
   tape(name, async (t) => {
     try {
       await handler(t)
     } catch(err) {
       t.fail(err)
       console.log(err.stack)
+    }
+    if(cleanup) {
+      await cleanup()
     }
     t.end()
   })
