c3dbb197023b4a4615d014fcd91ac7850a4135bf
Kai Davenport <kaiyadavenport@gmail.com>
2019-05-09 10:02:57 +0100

    mark test as passing if there is no error

--- test/asyncTest.js
+++ test/asyncTest.js
@@ -4,6 +4,7 @@ const asyncTest = (name, handler, cleanup) => {
   tape(name, async (t) => {
     try {
       await handler(t)
+      t.pass('there was no error')
     } catch(err) {
       t.fail(err)
       console.log(err.stack)
