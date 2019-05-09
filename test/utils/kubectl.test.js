422f742a9120b1e5c1f66dd71fa3749310d8bb33
Kai Davenport <kaiyadavenport@gmail.com>
2019-05-09 11:15:14 +0100

    test for kubectl errors throwing

--- test/utils/kubectl.test.js
+++ test/utils/kubectl.test.js
@@ -2,6 +2,7 @@
 
 const Kubectl = require('../../src/utils/kubectl')
 const asyncTest = require('../asyncTest')
+const asyncTestError = require('../asyncTestError')
 
 asyncTest('kubectl -> get help output', async (t) => {
 
@@ -9,4 +10,11 @@ asyncTest('kubectl -> get help output', async (t) => {
   const [ stdout, stderr ] = await kubectl.command(`help`)
 
   t.ok(stdout.toLowerCase().indexOf('kubernetes') >= 0, `the output contained the word Kubernetes`)
+})
+
+asyncTestError('kubectl -> run bad command', async (t) => {
+
+  const kubectl = Kubectl()
+  const [ stdout, stderr ] = await kubectl.command(`oranges`)
+
 })
