0889719effff6a48b8a2fa867795a943f0e5aa8c
Kai Davenport <kaiyadavenport@gmail.com>
2019-05-13 11:28:16 +0100

    full options for sawtooth 1.2 form

--- src/deployment_templates/sawtooth/1.2/form.js
+++ src/deployment_templates/sawtooth/1.2/form.js
@@ -28,21 +28,8 @@ const form = [
 
   [
     {
-      id: 'namespace',
-      title: `Kubernetes Namespace`,
-      helperText: 'The Kubernetes namespace',
-      component: 'text',
-      validate: {
-        type: 'string',
-        methods: [
-          ['required', 'Required'],
-          ['matches', `^[a-zA-Z0-9]+$`, 'Only alphanumeric characters']
-        ],
-      },
-    },
-    {
       id: 'network_name',
-      title: `Network Name`,
+      title: 'Network Name',
       helperText: 'The name of the sawtooth network',
       component: 'text',
       validate: {
@@ -53,12 +40,26 @@ const form = [
         ],
       },
     },
+    {
+      id: 'namespace',
+      title: 'Kubernetes Namespace',
+      helperText: 'The Kubernetes namespace',
+      component: 'text',
+      validate: {
+        type: 'string',
+        methods: [
+          ['required', 'Required'],
+          ['matches', `^[a-zA-Z0-9]+$`, 'Only alphanumeric characters']
+        ],
+      },
+    },
+
   ],
 
   [
     {
       id: 'dynamic_peering',
-      title: `Peering Type`,
+      title: 'Peering Type',
       helperText: 'Peering type for the validator',
       component: 'radio',
       row: true,
@@ -72,7 +73,7 @@ const form = [
     },
     {
       id: 'genesis_enabled',
-      title: `Genesis Block`,
+      title: 'Genesis Block',
       helperText: 'Should this network create the genesis block?',
       component: 'radio',
       row: true,
@@ -86,33 +87,139 @@ const form = [
     },
   ],
 
-  [
-    {
-      id: 'external_seeds',
-      title: 'External Seeds',
-      helperText: 'The list of external addresses to connect to',
-      list: {
-        mainField: 'address',
-        schema: [{
-          id: 'address',
-          title: `Seed address`,
-          helperText: 'Type the address of a new external seed (hostname:port or ip:port)',
-          component: 'text',
-          validate: {
-            type: 'string',
-            methods: [
-              ['required', 'Required'],
-            ],
-          },
-        }],
-        table: [{
-          title: 'Seed address',
-          name: 'address',
-        }]
-      }
-    },
+  
+  {
+    id: 'external_seeds',
+    title: 'External Seeds',
+    helperText: 'The list of external addresses to connect to',
+    list: {
+      mainField: 'address',
+      schema: [{
+        id: 'address',
+        title: 'Seed address',
+        helperText: 'Type the address of a new external seed (hostname:port or ip:port)',
+        component: 'text',
+        validate: {
+          type: 'string',
+          methods: [
+            ['required', 'Required'],
+          ],
+        },
+      }],
+      table: [{
+        title: 'Seed address',
+        name: 'address',
+      }]
+    }
+  },
 
-  ]
+  'Consensus Algorithm',
+
+  {
+    id: 'poet_enabled',
+    title: 'POET Enabled',
+    helperText: 'Should the POET consensus protocol be active on this network?',
+    component: 'radio',
+    row: true,
+    options: consensusOptions,
+    validate: {
+      type: 'string',
+      methods: [
+        ['required', 'Required']
+      ],
+    },
+  },
+
+  'Transaction Processors',
+
+  {
+    id: 'custom_tps',
+    title: 'Custom Transaction Processors',
+    helperText: 'Custom transaction processors to start and connect to the validator on tcp://localhost:4004',
+    list: {
+      mainField: 'name',
+      schema: [{
+        id: 'name',
+        title: 'Name',
+        helperText: 'The name of your custom transaction processor',
+        component: 'text',
+        validate: {
+          type: 'string',
+          methods: [
+            ['required', 'Required'],
+          ],
+        },
+      },{
+        id: 'image',
+        title: 'Image',
+        helperText: 'The docker image for your transaction processor',
+        component: 'text',
+        validate: {
+          type: 'string',
+          methods: [
+            ['required', 'Required'],
+          ],
+        },
+      },{
+        id: 'command',
+        title: 'Command',
+        helperText: 'The command for your transaction processor',
+        component: 'text',
+        validate: {
+          type: 'string',
+          methods: [
+            ['required', 'Required'],
+          ],
+        },
+      },{
+        id: 'args',
+        title: 'Arguments',
+        helperText: 'The arguments for your transaction processor',
+        component: 'text',
+        validate: {
+          type: 'string',
+          methods: [
+            ['required', 'Required'],
+          ],
+        },
+      }],
+      table: [{
+        title: 'Name',
+        name: 'name',
+      },{
+        title: 'Image',
+        name: 'image',
+      },{
+        title: 'Command',
+        name: 'command',
+      },{
+        title: 'Arguments',
+        name: 'args',
+      }]
+    }
+  },
+
+  {
+    id: 'additional_tps',
+    title: 'Additional Transaction Processors',
+    component: 'multipleCheckbox',
+    helperText: 'Choose which additional transaction processors to activate',
+    options: [{
+      title: 'RBAC',
+      value: 'rabc',
+    }, {
+      title: 'XO',
+      value: 'xo',
+    }, {
+      title: 'Smallbank',
+      value: 'smallbank',
+    }, {
+      title: 'Simple',
+      value: 'simple',
+    }]
+  },
+
+
 ]
 
 module.exports = form
