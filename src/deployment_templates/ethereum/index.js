71c3c33f36f9be47936c3046dc3259829715ccc9
Kai Davenport <kaiyadavenport@gmail.com>
2019-05-13 11:40:42 +0100

    expose the forms with the versions corresponding to the template folders

--- src/deployment_templates/ethereum/index.js
+++ src/deployment_templates/ethereum/index.js
@@ -2,13 +2,13 @@ const geth_1_8 = require('./geth-1.8/form')
 
 module.exports = {
   forms: {
-    geth_1_8,
+    'geth-1.8': geth_1_8,
   },
   button: {
     title: 'Ethereum',
     versions: [{
       title: 'Geth 1.8',
-      form: 'geth_1_8',
+      form: 'geth-1.8',
     }],
   },
 }
--- src/deployment_templates/sawtooth/index.js
+++ src/deployment_templates/sawtooth/index.js
@@ -2,13 +2,13 @@ const version_1_2 = require('./1.2/form')
 
 module.exports = {
   forms: {
-    version_1_2,
+    '1.2': version_1_2,
   },
   button: {
     title: 'Sawtooth',
     versions: [{
       title: 'Version 1.2',
-      form: 'version_1_2',
+      form: '1.2',
     }],
   },
 }
