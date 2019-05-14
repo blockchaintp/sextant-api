271d53f63408ac03310b41213e14118fa0215154
Kai Davenport <kaiyadavenport@gmail.com>
2019-05-14 08:51:51 +0100

    merge values from deployment into template defaults

--- src/tasks/deployment/update.js
+++ src/tasks/deployment/update.js
@@ -1,3 +1,4 @@
+const templateUtils = require('../../deployment_templates/utils')
 const saveAppliedState = require('./utils/saveAppliedState')
 
 const DeploymentUpdate = ({
@@ -12,9 +13,25 @@ const DeploymentUpdate = ({
 
   const id = task.resource_id
 
-  const deployment = yield store.cluster.get({
+  const deployment = yield store.deployment.get({
     id,
   }, trx)
+
+  console.log('--------------------------------------------')
+  console.log('--------------------------------------------')
+  console.dir(deployment)
+
+  const {
+    deployment_type,
+    deployment_version,
+    desired_state,
+  } = deployment
+
+  const templateData = yield templateUtils.getTemplateData({
+    deployment_type,
+    deployment_version,
+    desired_state,
+  })
 
   yield saveAppliedState({
     id,
