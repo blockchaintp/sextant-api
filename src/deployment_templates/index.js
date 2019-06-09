ee7f15469ad4e2bddb03909f9ec99e8518aa3117
Kai Davenport <kaiyadavenport@gmail.com>
2019-06-09 14:41:29 +0100

    hide the ethereum deployment until we have it ready
    Signed-off-by: Kai Davenport <kaiyadavenport@gmail.com>

--- src/deployment_templates/index.js
+++ src/deployment_templates/index.js
@@ -1,7 +1,7 @@
 const sawtooth = require('./sawtooth')
-const ethereum = require('./ethereum')
+//const ethereum = require('./ethereum')
 
 module.exports = {
   sawtooth,
-  ethereum,
+  //ethereum,
 }
