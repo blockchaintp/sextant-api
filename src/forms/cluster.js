fd0f51956e669e05ec33347f92dcca713ec16dbc
Mikeala Sheldt <mikaela@blockchaintp.com>
2019-09-11 16:09:55 -0400

    SXT-101 Add extra '\' characters to regEx
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/forms/cluster.js
+++ src/forms/cluster.js
@@ -8,7 +8,7 @@ const validators = {
   ],
   ca: [
     'matches',
-    ['^-----BEGIN CERTIFICATE-----[\s\S]*-----END CERTIFICATE-----$'],
+    ['^-----BEGIN CERTIFICATE-----[\\s\\S]*-----END CERTIFICATE-----$'],
     {
       message: 'Must be a valid certificate',
       excludeEmptyString: true,
