09f605b1314e4d2577dc01c1da1b102ef96fb7eb
Mikeala Sheldt <mikaela@blockchaintp.com>
2019-09-26 16:41:37 -0400

    release-changes Add settings table schema
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

---
+++ migrations/201909261759_settings.js
@@ -0,0 +1,21 @@
+const up = (knex, Promise) => {
+  return Promise.all([
+    knex.schema.createTable('settings', (table) => {
+      table.specificType('id', 'serial primary key not null')
+      table.specificType('created_at', 'timestamp default now()')
+      table.string('key').notNullable()
+      table.text('value').notNullable()
+    })
+  ])
+}
+
+const down = (knex, Promise) => {
+  return Promise.all([
+    knex.schema.dropTable('settings')
+  ])
+}
+
+module.exports = {
+  up,
+  down
+}
