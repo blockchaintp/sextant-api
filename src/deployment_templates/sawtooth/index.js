fa73d7f4a95d0cf46f7dd58b40cb2f3ca37fd93d
Mikeala Sheldt <mikaela@blockchaintp.com>
2019-09-27 14:11:29 -0400

    release-changes Remove Sawtooth 1.0 option
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/deployment_templates/sawtooth/index.js
+++ src/deployment_templates/sawtooth/index.js
@@ -7,21 +7,21 @@ const version_1_2_summary = require('./1.2/summary')
 
 module.exports = {
   forms: {
-    '1.0': version_1_0_form,
+    //'1.0': version_1_0_form,
     '1.1': version_1_1_form,
     //'1.2': version_1_2_form,
   },
   summary: {
-    '1.0': version_1_0_summary,
+    //'1.0': version_1_0_summary,
     '1.1': version_1_1_summary,
     //'1.2': version_1_2_summary,
   },
   // paths to specific fields in the deployment
   paths: {
-    '1.0': {
-      name: 'sawtooth.networkName',
-      namespace: 'sawtooth.namespace',
-    },
+    // '1.0': {
+    //   name: 'sawtooth.networkName',
+    //   namespace: 'sawtooth.namespace',
+    // },
     '1.1': {
       name: 'sawtooth.networkName',
       namespace: 'sawtooth.namespace',
@@ -34,11 +34,13 @@ module.exports = {
   button: {
     title: 'Sawtooth',
     icon: "/thirdParty/hyperledger-sawtooth.png",
-    versions: [{
-      title: 'Version 1.0 (BTP1)',
-      form: '1.0',
-      description: "Summary place holder text about this volume of awesomeness.",
-    },{
+    versions: [
+    //   {
+    //   title: 'Version 1.0 (BTP1)',
+    //   form: '1.0',
+    //   description: "Summary place holder text about this volume of awesomeness.",
+    // },
+    {
       title: 'Version 1.1 (BTP2)',
       form: '1.1',
       description: "Summary place holder text about this volume of awesomeness.",
