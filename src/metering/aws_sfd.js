74dc75e7138338edfa214777b05c085927e3519a
Mikeala Sheldt <mikaela@blockchaintp.com>
2019-10-11 14:22:22 -0400

    SXT-205 Fetch region for metering
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/metering/aws_sfd.js
+++ src/metering/aws_sfd.js
@@ -1,4 +1,5 @@
 'use strict'
+const axios = require('axios');
 
 const AWS = require('aws-sdk');
 const pino = require('pino')({
@@ -12,29 +13,35 @@ const PUBLIC_KEY_VERSION = 1
 // 10 minutes
 const LOOP_DELAY = 1000 * 60 * 10
 
-const marketplacemetering = new AWS.MarketplaceMetering({
-
-})
-
-const registerUsage = () => {
-  marketplacemetering.registerUsage({
-    ProductCode: PRODUCT_CODE,
-    PublicKeyVersion: PUBLIC_KEY_VERSION,
-  }, (err, result) => {
-    if(err) {
-      pino.error({
-        type: 'registerUsage',
-        error: err,
+// get region of aws cluster
+axios.get('http://169.254.169.254/latest/dynamic/instance-identity/document')
+    .then(response => {
+      const marketplacemetering = new AWS.MarketplaceMetering({
+        region: response.data.region
       })
-    }
-    else {
-      pino.info({
-        type: 'registerUsage',
-        result,
-      })
-    }
-  })
-}
+      const registerUsage = () => {
+        marketplacemetering.registerUsage({
+          ProductCode: PRODUCT_CODE,
+          PublicKeyVersion: PUBLIC_KEY_VERSION,
+        }, (err, result) => {
+          if(err) {
+            pino.error({
+              type: 'registerUsage',
+              error: err,
+            })
+          }
+          else {
+            pino.info({
+              type: 'registerUsage',
+              result,
+            })
+          }
+        })
+      }
 
-registerUsage()
-setInterval(registerUsage, LOOP_DELAY)
+      registerUsage()
+      setInterval(registerUsage, LOOP_DELAY)
+    })
+    .catch(error => {
+      console.log(error);
+    });
