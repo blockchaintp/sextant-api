7bb7138fb5b41b675b47fbff59e11867ca41e575
Mikeala Sheldt <mikaela@blockchaintp.com>
2019-10-15 16:29:52 -0400

    SXT-210 Create editions directory for edition details
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

---
+++ editions/aws_sfs.js
@@ -0,0 +1,16 @@
+// Edition object for Sawtooth
+
+const edition = {
+  metering: {
+    type: 'aws',
+    productCode: '965zq9jyoo7ry5e2cryolgi2l',
+    publicKeyVersion: 1,
+  },
+  headerText: 'Sextant for Sawtooth',
+  headerLogo: "path/to/assest/and/logo",
+
+}
+
+module.exports = {
+  edition
+}
---
+++ editions/dev.js
@@ -0,0 +1,13 @@
+// Edition object for dev mode
+
+const edition = {
+  metering: {
+    type: 'dev'
+  },
+  headerText: 'Sextant Dev Mode',
+  headerLogo: "path/to/assest/and/logo"
+}
+
+module.exports = {
+  edition
+}
