636bda410a9d9b9ddeba38f24dbce2bb2fc44247
Kevin O'Donnell <kevin@blockchaintp.com>
2020-01-22 12:05:30 -0500

    Initial add of daml-on-qldb templates
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- src/deployment_templates/daml/index.js
+++ src/deployment_templates/daml/index.js
@@ -4,14 +4,19 @@ const version_1_1_summary = require('./daml-on-sawtooth/summary')
 const daml_on_postgres_form = require('./daml-on-postgres/form')
 const daml_on_postgres_summary = require('./daml-on-postgres/summary')
 
+const daml_on_qldb_form = require('./daml-on-qldb/form')
+const daml_on_qldb_summary = require('./daml-on-qldb/summary')
+
 module.exports = {
   forms: {
     'daml-on-sawtooth': version_1_1_form,
     'daml-on-postgres': daml_on_postgres_form,
+    'daml-on-qldb': daml_on_qldb_form,
   },
   summary: {
     'daml-on-sawtooth': version_1_1_summary,
     'daml-on-postgres': daml_on_postgres_summary,
+    'daml-on-qldb': daml_on_qldb_summary,
   },
   // paths to specific fields in the deployment
   paths: {
@@ -23,13 +28,17 @@ module.exports = {
       name: 'deployment.name',
       namespace: 'deployment.namespace',
     },
+    'daml-on-qldb': {
+      name: 'deployment.name',
+      namespace: 'deployment.namespace',
+    },
   },
   button: {
     versions: [
       {
         title: 'DAML on Sawtooth',
         icon: "/thirdParty/daml.png",
-        version: 'daml-on-sawtooth(v0.1.3), BTP Sawtooth(BTP2.0)',
+        version: 'daml-on-sawtooth(~0.1.4), BTP Sawtooth(BTP2.0)',
         form: 'daml-on-sawtooth',
         description: "The DAML smart contract runtime engine with Hyperledger Sawtooth as the backing DLT.",
         features: [
@@ -39,9 +48,19 @@ module.exports = {
       },{
         title: 'DAML on Aurora',
         icon: "/thirdParty/daml.png",
-        version: 'daml-on-postgres(v0.1.3), AWS Aurora(Postgresql)',
+        version: 'daml-on-postgres(~0.13.37), AWS Aurora(Postgresql)',
         form: 'daml-on-postgres',
         description: "The DAML smart contract runtime engine with AWS Aurora(Postgresql) as the backing DLT.",
+        features: [
+          "daml.parties",
+          "daml.archives"
+        ]
+      },{
+        title: 'DAML on QLDB',
+        icon: "/thirdParty/daml.png",
+        version: 'daml-on-qldb(Early Access), AWS QLDB',
+        form: 'daml-on-qldb',
+        description: "The DAML smart contract runtime engine with AWS QLDB as the backing DLT.",
         features: [
           "daml.parties",
           "daml.archives"
