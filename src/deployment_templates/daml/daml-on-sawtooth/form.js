f097dc701dbcfed3fdc82b6d778a2bdf0b4db7bc
Kevin O'Donnell <kevin@blockchaintp.com>
2020-02-05 16:59:44 -0500

    Separate daml-rpc into a pod and service
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- src/deployment_templates/daml/daml-on-sawtooth/form.js
+++ src/deployment_templates/daml/daml-on-sawtooth/form.js
@@ -43,22 +43,6 @@ const form = [
 
   [
     {
-      id: 'sawtooth.dynamicPeering',
-      title: 'Peering Type',
-      helperText: 'Peering type for the validator',
-      component: 'radio',
-      default: true,
-      dataType: 'boolean',
-      row: true,
-      options: options.peering,
-      validate: {
-        type: 'string',
-        methods: [
-          ['required', 'Required']
-        ],
-      },
-    },
-    {
       id: 'sawtooth.genesis.enabled',
       title: 'Genesis Block',
       helperText: 'Should this network create the genesis block?',
