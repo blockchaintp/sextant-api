5dbf4facb271c13014cbd72671042643a32435ca
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-06-01 10:44:56 -0400

    HELM2 add helmRepos and ChartTable to edition.js
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- editions/dev.js
+++ editions/dev.js
@@ -1,4 +1,6 @@
 // Edition object for dev mode
+const dotenv = require("dotenv");
+dotenv.config();
 
 const edition = {
   deployment: {
@@ -7,6 +9,27 @@ const edition = {
   metering: {
     type: 'dev'
   },
+  helmRepos: [
+    {
+      name: 'btp-dev',
+      url: 'https://dev.catenasys.com/repository/catenasys-helm-dev/',
+      username: process.env.BTP_DEV_USERNAME,
+      password: process.env.BTP_DEV_PASSWORD,
+      charts: ['sawtooth', /* 'daml-on-sawtooth', 'daml-on-postgres', 'daml-on-qldb' */]
+    }
+  ],
+  // ******* we ought to think about how to include chart versions as well
+  chartTable: {
+    sawtooth: {
+      1.1: { chart: 'btp-dev/sawtooth', extension: 'sawtooth' }
+    },
+    /*daml: {
+      'daml-on-postgres': {chart: 'btp-dev/daml-on-postgres', exstension: 'daml-on-postgres'},
+      'daml-on-qldb': 'btp-dev/daml-on-qldb',
+      'daml-on-sawtooth': 'btp-dev/daml-on-sawtooth'
+    }*/
+  },
+
 }
 
 module.exports = {
