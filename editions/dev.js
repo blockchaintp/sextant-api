5b47a7d63175d2ec5f85ef6374a5c2990933924c
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-06-25 09:02:32 -0400

    HELM2 Remove commented code
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- editions/dev.js
+++ editions/dev.js
@@ -15,19 +15,13 @@ const edition = {
       url: 'https://dev.catenasys.com/repository/catenasys-helm-dev/',
       username: process.env.BTP_DEV_USR,
       password: process.env.BTP_DEV_PSW,
-      charts: ['sawtooth', /* 'daml-on-sawtooth', 'daml-on-postgres', 'daml-on-qldb' */]
+      charts: ['sawtooth', ]
     }
   ],
-  // ******* we ought to think about how to include chart versions as well
   chartTable: {
     sawtooth: {
       1.1: { chart: 'btp-dev/sawtooth', extension: 'sawtooth' }
     },
-    /*daml: {
-      'daml-on-postgres': {chart: 'btp-dev/daml-on-postgres', exstension: 'daml-on-postgres'},
-      'daml-on-qldb': 'btp-dev/daml-on-qldb',
-      'daml-on-sawtooth': 'btp-dev/daml-on-sawtooth'
-    }*/
   },
 
 }
