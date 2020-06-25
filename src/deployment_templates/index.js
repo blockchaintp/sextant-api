3b5b8248b0a5b93ef28d101d300e1ed7a96c77f6
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-06-25 12:42:02 -0400

    HELM3 Remove commented code
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/deployment_templates/index.js
+++ src/deployment_templates/index.js
@@ -1,13 +1,3 @@
-// const { edition } = require('../edition')
-// const { TemplateLoader } = require('./templateLoader')
-
-// const deployment = edition.deployment
-// const templateLoader = new TemplateLoader(deployment.types)
-
-// module.exports = templateLoader.load()
-
 const { mergedDeploymentDetails} = require('./templateLoader')
-
-console.log("DETAILS\n\n", mergedDeploymentDetails());
 
 module.exports = mergedDeploymentDetails()
