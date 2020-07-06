876728e062e758f2d7dfe142128598a35f9ab940
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-07-06 09:21:24 -0400

    SXT-313 Validate QLDB ledger length max is 32 characters
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/deployment_templates/daml/daml-on-qldb/form.js
+++ src/deployment_templates/daml/daml-on-qldb/form.js
@@ -54,7 +54,7 @@ const form = [
         type: 'string',
         methods: [
           ['required', 'Required'],
-          ['matches', [`^[a-zA-Z]([-a-zA-Z0-9]*[a-zA-Z0-9])*$`], "a ledger id must consist of alphanumeric characters or '-', and must start and end with an alphanumeric character"]
+          ['matches', [`^[a-zA-Z]([-a-zA-Z0-9]*[a-zA-Z0-9]){0,31}$`], "a ledger id must consist of alphanumeric characters or '-', and must start and end with an alphanumeric character"]
         ],
       },
     },
