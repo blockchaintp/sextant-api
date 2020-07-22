fb41343c513b28238ee98897d8c59e75c814c1ab
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-07-22 10:42:19 -0400

    refactor(utils): remove getDeploymentMethod from helmUtils and place adjacent to invocation
    better for code readability
    
    sxt-427
    
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/tasks/deployment/utils/helmUtils.js
+++ src/tasks/deployment/utils/helmUtils.js
@@ -1,26 +1,4 @@
 
-/*
-This function relies on the chartTable in the edition object 
-Using the deployment type and version, determine whether or not the template type is helm or classic
-The template type will always default to 'classic'
-*/
-
-const { edition } = require('../../../edition')
-
-const getDeploymentMethod = (deployment_type, deployment_version) => {
-
-  const chartTable = edition.chartTable
-  let deploymentMethod
-
-  if (chartTable && chartTable[deployment_type] && chartTable[deployment_type][deployment_version]) {
-    deploymentMethod = 'helm'
-  } else {
-    deploymentMethod = 'classic'
-  }
-
-  return deploymentMethod
-}
-
 const getChartInfo = (deployment_type, deployment_version) => {
   const chartTable = edition.chartTable
   
@@ -29,4 +7,4 @@ const getChartInfo = (deployment_type, deployment_version) => {
   return chartInfo
 }
 
-module.exports = { getDeploymentMethod, getChartInfo }
+module.exports = { getChartInfo }
