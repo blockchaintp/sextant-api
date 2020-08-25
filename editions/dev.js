bbd210840f5133b671805aca3075c8d8057efddc
Kevin O'Donnell <kevin@blockchaintp.com>
2020-08-25 18:25:40 -0400

    fix(editions): add daml-on-besu as separate item rather than in daml
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- editions/dev.js
+++ editions/dev.js
@@ -6,7 +6,16 @@ dotenv.config();
 const edition = {
   deployment: {
     classic: ["daml", "taekion"],
-    helm: ["sawtooth", "openebs","daml-on-postgres","fluentd","elasticsearch","kibana","besu"],
+    helm: [
+      "besu",
+      "daml-on-besu", 
+      "daml-on-postgres", 
+      "elasticsearch", 
+      "fluentd", 
+      "kibana", 
+      "openebs",
+      "sawtooth"
+    ],
   },
   metering: {
     type: "dev",
@@ -15,33 +24,42 @@ const edition = {
     {
       name: "btp-unstable",
       url: "https://btp-charts-unstable.s3.amazonaws.com/charts",
-      charts: ["sawtooth","daml-on-besu","daml-on-postgres","openebs","fluentd","elasticsearch","kibana","besu"],
+      charts: [
+        "besu",
+        "daml-on-besu", 
+        "daml-on-postgres", 
+        "elasticsearch", 
+        "fluentd", 
+        "kibana", 
+        "openebs", 
+        "sawtooth", 
+      ],
     },
   ],
   chartTable: {
     sawtooth: {
       1.1: { chart: "btp-unstable/sawtooth", extension: "sawtooth" },
     },
-    daml: {
-      "daml-on-besu": { chart: "btp-unstable/daml-on-besu", extension: "daml" },
+    "daml-on-besu": {
+      1.3: { chart: "btp-unstable/daml-on-besu", extension: "daml" },
     },
     "daml-on-postgres": {
       1.3: { chart: "btp-unstable/daml-on-postgres", extension: "daml" },
     },
     openebs: {
-      2.0: { chart: "btp-unstable/openebs", extension: "openebs"},
+      2.0: { chart: "btp-unstable/openebs", extension: "openebs" },
     },
     fluentd: {
-      1.11: { chart: "btp-unstable/fluentd", extension: "fluentd"},
+      1.11: { chart: "btp-unstable/fluentd", extension: "fluentd" },
     },
     elasticsearch: {
-      7.9: { chart: "btp-unstable/elasticsearch", extension: "elasticsearch"},
+      7.9: { chart: "btp-unstable/elasticsearch", extension: "elasticsearch" },
     },
     kibana: {
-      7.8: { chart: "btp-unstable/kibana", extension: "kibana"},
+      7.8: { chart: "btp-unstable/kibana", extension: "kibana" },
     },
     besu: {
-      1.4: { chart: "btp-unstable/besu", extension: "besu"},
+      1.4: { chart: "btp-unstable/besu", extension: "besu" },
     }
   },
 };
