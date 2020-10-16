8c695a8707f4aa8cbda56fb8cec41e7bed4f9cf4
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-10-16 16:23:11 -0400

    refactor(utils): remove volume list response data processing util
    sxt-484
    
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/utils/taekion.js
+++ src/utils/taekion.js
@@ -1,23 +1,9 @@
-const processVolumeResponse = (data) => {
-  return data.payload.reduce((all, entry) => {
-    return all.concat(Object.keys(entry).map(name => {
-      const data = entry[name]
-      return Object.assign({}, data, {
-        name,
-      })
-    }))
-  }, [])
-}
-
-const processSnapshotResponse = (data) => data.payload.map(entry => {
-  return {
-    volume: entry.VolumeUuid,
-    name: entry.Name,
-    block: entry.Block,
-  }
-})
+const processSnapshotResponse = (data) => data.payload.map((entry) => ({
+  volume: entry.VolumeUuid,
+  name: entry.Name,
+  block: entry.Block,
+}))
 
 module.exports = {
-  processVolumeResponse,
   processSnapshotResponse,
 }
