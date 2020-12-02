a61383355d3ba10fc72a567c48b3bdb7550a63e4
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-12-02 13:24:00 -0500

    fix(dev-edition): fix typo and use unstable images
    sxt-509
    
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- editions/dev.js
+++ editions/dev.js
@@ -56,55 +56,55 @@ const edition = {
   ],
   chartTable: {
     besu: {
-      1.4: { chart: 'btp-stable/besu', order: 1, extension: 'besu' },
+      1.4: { chart: 'btp-unstable/besu', order: 1, extension: 'besu' },
     },
     sawtooth: {
-      1.1: { chart: 'btp-stable/sawtooth', order: 2, extension: 'sawtooth' },
+      1.1: { chart: 'btp-unstable/sawtooth', order: 2, extension: 'sawtooth' },
     },
     'daml-on-besu': {
-      1.3: { chart: 'btp-stable/daml-on-besu', order: 3, extension: 'daml' },
+      1.3: { chart: 'btp-unstable/daml-on-besu', order: 3, extension: 'daml' },
     },
     'daml-on-sawtooth': {
-      1.3: { chart: 'btp-stable/daml-on-sawtooth', order: 4, extension: 'daml' },
+      1.3: { chart: 'btp-unstable/daml-on-sawtooth', order: 4, extension: 'daml' },
     },
     'daml-on-qldb': {
-      1.3: { chart: 'btp-stable/daml-on-qldb', order: 5, extension: 'daml' },
+      1.3: { chart: 'btp-unstable/daml-on-qldb', order: 5, extension: 'daml' },
     },
     'daml-on-postgres': {
-      1.3: { chart: 'btp-stable/daml-on-postgres', order: 6, extension: 'daml' },
+      1.3: { chart: 'btp-unstable/daml-on-postgres', order: 6, extension: 'daml' },
     },
     'tfs-on-sawtooth': {
       0.1: { chart: 'btp-unstable/tfs-on-sawtooth', order: 7, extension: 'tfs' },
     },
     sextant: {
-      2.1: { chart: 'btp-stable/sextant', order: 8, extension: 'sextant' },
+      2.1: { chart: 'btp-unstable/sextant', order: 8, extension: 'sextant' },
     },
     openebs: {
-      '2.0': { chart: 'btp-stable/openebs', order: 9, extension: 'openebs' },
+      '2.0': { chart: 'btp-unstable/openebs', order: 9, extension: 'openebs' },
     },
     fluentd: {
-      1.11: { chart: 'btp-stable/fluentd', order: 10, extension: 'fluentd' },
+      1.11: { chart: 'btp-unstable/fluentd', order: 10, extension: 'fluentd' },
     },
     elasticsearch: {
-      7.9: { chart: 'btp-stable/elasticsearch', order: 11, extension: 'elasticsearch' },
+      7.9: { chart: 'btp-unstable/elasticsearch', order: 11, extension: 'elasticsearch' },
     },
     kibana: {
-      7.8: { chart: 'btp-stable/kibana', order: 12, extension: 'kibana' },
+      7.8: { chart: 'btp-unstable/kibana', order: 12, extension: 'kibana' },
     },
     'nginx-ingress': {
-      1.8: { chart: 'btp-stable/nginx-ingress', order: 13, extension: 'ingress' },
+      1.8: { chart: 'btp-unstable/nginx-ingress', order: 13, extension: 'ingress' },
     },
     grafana: {
-      7.1: { chart: 'btp-stable/grafana', order: 14, extension: 'grafana' },
+      7.1: { chart: 'btp-unstable/grafana', order: 14, extension: 'grafana' },
     },
     vault: {
-      1.5: { chart: 'btp-stable/vault', order: 15, extension: 'vault' },
+      1.5: { chart: 'btp-unstable/vault', order: 15, extension: 'vault' },
     },
     influxdb: {
-      1.8: { chart: 'btp-stable/influxdb', order: 16, extension: 'influxdb' },
+      1.8: { chart: 'btp-unstable/influxdb', order: 16, extension: 'influxdb' },
     },
     'postgresql-ha': {
-      11.9: { chart: 'btp-stable/postgresql-ha', order: 17, extension: 'pgsql' },
+      11.9: { chart: 'btp-unstable/postgresql-ha', order: 17, extension: 'pgsql' },
     },
   },
 };
