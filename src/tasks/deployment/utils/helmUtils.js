8b8866d401363ccc05c22bc0f9cc84145dd9855b
Mikeala Sheldt <mikaela@blockchaintp.com>
2021-02-03 09:59:37 -0500

    feat(helm-util): add utility function for determining version of untarred chart
    sxt-472
    
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/tasks/deployment/utils/helmUtils.js
+++ src/tasks/deployment/utils/helmUtils.js
@@ -1,4 +1,12 @@
+const yaml = require('js-yaml')
+const fs = require('fs')
+const path = require('path')
+
+const readFile = fs.readFileSync
+
 const { edition } = require('../../../edition')
+
+const HELM_CHARTS_PATH = path.resolve(__dirname, './../../helmCharts')
 
 const getChartInfo = (deployment_type, deployment_version) => {
   const { chartTable } = edition
@@ -8,4 +16,16 @@ const getChartInfo = (deployment_type, deployment_version) => {
   return chartInfo
 }
 
-module.exports = { getChartInfo }
+const getYaml = (filepath) => {
+  const yamlContent = readFile(filepath, 'utf8')
+  return yaml.safeLoad(yamlContent, { schema: yaml.FAILSAFE_SCHEMA })
+}
+
+const getChartVersion = (deploymentType, deploymentVersion) => {
+  const Chart = getYaml(`${HELM_CHARTS_PATH}/${deploymentType}/${deploymentVersion}/Chart.yaml`)
+  const { version } = Chart
+
+  return version
+}
+
+module.exports = { getChartInfo, getChartVersion }
