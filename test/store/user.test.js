e7db842b63152ebfc1d0ea371b6a096154762792
Mikeala Sheldt <mikaela@blockchaintp.com>
2021-06-03 10:48:03 -0400

    refactor(test): linting
    sxt-655
    
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- test/store/user.test.js
+++ test/store/user.test.js
@@ -1,7 +1,3 @@
-'use strict'
-
-const tape = require('tape')
-const async = require('async')
 const tools = require('../tools')
 const asyncTest = require('../asyncTest')
 
@@ -13,20 +9,17 @@ const enumerations = require('../../src/enumerations')
 
 const UserStore = require('../../src/store/user')
 
-database.testSuiteWithDatabase(getConnection => {
-
+database.testSuiteWithDatabase((getConnection) => {
   let userMap = {}
 
   asyncTest('user store -> list no data', async (t) => {
-
     const store = UserStore(getConnection())
-  
+
     const users = await store.list({})
-    t.equal(users.length, 0, `there were no users`)
+    t.equal(users.length, 0, 'there were no users')
   })
 
   asyncTest('user store -> create with missing values', async (t) => {
-
     const store = UserStore(getConnection())
 
     await tools.insertWithMissingValues(t, store, {
@@ -38,9 +31,8 @@ database.testSuiteWithDatabase(getConnection => {
   })
 
   asyncTest('user store -> create with bad role', async (t) => {
-  
     const store = UserStore(getConnection())
-  
+
     let error = null
 
     try {
@@ -49,36 +41,33 @@ database.testSuiteWithDatabase(getConnection => {
           username: 'apples',
           hashed_password: 'na',
           server_side_key: 'na',
-          permission: 'apples'
-        }
+          permission: 'apples',
+        },
       })
-    }
-    catch(err) {
+    } catch (err) {
       error = err
     }
-    t.ok(error, `there was an error`) 
+    t.ok(error, 'there was an error')
   })
-  
+
   asyncTest('user store -> create users', async (t) => {
     const users = await fixtures.insertTestUsers(getConnection())
-    t.deepEqual(users.admin.meta, {}, `the metadata defaults to empty object`)
+    t.deepEqual(users.admin.meta, {}, 'the metadata defaults to empty object')
     userMap = users
   })
-  
+
   asyncTest('user store -> list with ordered data', async (t) => {
-  
     const store = UserStore(getConnection())
 
     const correctOrder = [].concat(enumerations.USER_TYPES)
     correctOrder.sort()
-  
+
     const users = await store.list({})
     t.equal(users.length, fixtures.SIMPLE_USER_DATA.length, `there were ${fixtures.SIMPLE_USER_DATA.length} users`)
-    t.deepEqual(users.map(user => user.username), correctOrder, 'the users were in the correct order')
+    t.deepEqual(users.map((user) => user.username), correctOrder, 'the users were in the correct order')
   })
-  
+
   asyncTest('user store -> get from username then id', async (t) => {
-  
     const store = UserStore(getConnection())
 
     const usernameUser = await store.get({
@@ -89,39 +78,36 @@ database.testSuiteWithDatabase(getConnection => {
       id: usernameUser.id,
     })
 
-    t.equal(usernameUser.username, config.USER_TYPES.admin, `the returned username is correct`)
-    t.equal(idUser.username, config.USER_TYPES.admin, `the returned username is correct`)
+    t.equal(usernameUser.username, config.USER_TYPES.admin, 'the returned username is correct')
+    t.equal(idUser.username, config.USER_TYPES.admin, 'the returned username is correct')
   })
-  
+
   asyncTest('user store -> update user', async (t) => {
-  
     const store = UserStore(getConnection())
-  
+
     const updateUser = await store.update({
       id: userMap[config.USER_TYPES.admin].id,
       data: {
         username: 'oranges',
-      }
+      },
     })
 
     const getUser = await store.get({
       username: 'oranges',
     })
 
-    t.equal(updateUser.username, 'oranges', `the new username is correct`)
-    t.equal(updateUser.id, getUser.id, `querying on the updated user is working`)    
+    t.equal(updateUser.username, 'oranges', 'the new username is correct')
+    t.equal(updateUser.id, getUser.id, 'querying on the updated user is working')
   })
-  
+
   asyncTest('user store -> delete user', async (t) => {
-  
     const store = UserStore(getConnection())
-  
+
     await store.delete({
       id: userMap[config.USER_TYPES.admin].id,
     })
-    
+
     const users = await store.list({})
-    t.equal(users.length, fixtures.SIMPLE_USER_DATA.length - 1, `there is 1 less user`)
+    t.equal(users.length, fixtures.SIMPLE_USER_DATA.length - 1, 'there is 1 less user')
   })
-  
 })
