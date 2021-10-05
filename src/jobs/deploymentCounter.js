cb98db0060abc52527d26b60dae71bf20889f1f7
Mikeala Sheldt <mikaela@blockchaintp.com>
2021-10-05 11:53:52 -0400

    feat(jobs): add job that counts he number of active deployments in the db
    active deployments are stored at provisioned in the db
    
    sxt-833
    
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>
    
    SXT-833
    
    SXT-833

---
+++ src/jobs/deploymentCounter.js
@@ -0,0 +1,18 @@
+const logger = require('../logging').getLogger({
+  name: 'jobs',
+  job: 'Deployment Counter',
+})
+
+const deploymentCounter = async (store) => {
+  // checks the DB for active deployments and returns the number
+  const activeDeployments = await store.deployment.listActive()
+  if (!activeDeployments) return []
+  const deploymentNames = activeDeployments.map((deployment) => deployment.name)
+  logger.info({
+    output: deploymentNames,
+    count: deploymentNames.length,
+  })
+  return activeDeployments
+}
+
+module.exports = deploymentCounter
