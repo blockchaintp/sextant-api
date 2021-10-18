b9459ccf2202ad99871a0538635d7313c0ebed58
Kevin O'Donnell <kevin@blockchaintp.com>
2021-10-18 19:39:59 -0400

    fix(utils): logg the keypair that was fetched
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- src/utils/sextantKeyPair.js
+++ src/utils/sextantKeyPair.js
@@ -1,21 +1,22 @@
 const secp256k1 = require('./secp256k1')
 const base64 = require('./base64')
 
+const logger = require('../logging').getLogger({
+  name: 'utils/sextantKeyPair',
+})
+
 const create = async ({
   store,
   deployment,
 }, trx) => {
-
   const keyPair = secp256k1.binaryToHex(secp256k1.createKeyPair())
-  await store.deploymentsecret.create({
+  return store.deploymentsecret.create({
     data: {
       deployment,
       name: 'sextantKeypair',
       rawData: JSON.stringify(keyPair),
-    }
+    },
   }, trx)
-
-  return keyPair
 }
 
 // load the sextant keypair for a deployment
@@ -27,8 +28,8 @@ const get = async ({
     deployment,
     name: 'sextantKeypair',
   }, trx)
-  const keyPair = JSON.parse(base64.decode(secret.base64data))
-  return keyPair
+  logger.trace({ secret }, 'Sextant KeyPair fetched')
+  return JSON.parse(base64.decode(secret.base64data))
 }
 
 module.exports = {
