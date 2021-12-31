3cb853ce012d619c061ce0eee4854c448f423f77
Kevin O'Donnell <kevin@blockchaintp.com>
2021-12-31 00:25:19 -0500

    style(jobs): sonar issues
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- src/jobs/meter/AwsMeterUsage.js
+++ src/jobs/meter/AwsMeterUsage.js
@@ -1,5 +1,6 @@
 const Promise = require('bluebird');
 const { MarketplaceMeteringClient, MeterUsageCommand } = require('@aws-sdk/client-marketplace-metering');
+const { sendCommandOrFail } = require('./AwsMeterUtils');
 const { DEPLOYMENT_STATUS } = require('../../config');
 const AWS = require('../../utils/aws');
 const ClusterKubectl = require('../../utils/clusterKubectl');
@@ -160,61 +161,7 @@ class AwsMeterUsage extends AbstractJob {
       logger.info({ params }, `${this.getName()} command for ${currentHour()}`);
       return new MeterUsageCommand(params)
     })
-    await commands.map((command) => this.sendCommandOrFail(command, client))
-  }
-
-  async sendCommandOrFail(command, client) {
-    try {
-      logger.info(`${this.getName()} send command for ${currentHour()}`);
-      const data = await client.send(command);
-      logger.info({ data }, `${this.getName()} MeterUsageCommand executed successfully`);
-    } catch (error) {
-      logger.error({ error }, `${this.getName()} failed`);
-
-      switch (error.name) {
-        case 'UnrecognizedClientException':
-        case 'CredentialsProviderError':
-        case 'CustomerNotEntitledException':
-          this.shutdownProcess('You are not entitled to use this container in your account');
-          break;
-        case 'DisabledApiException':
-          this.shutdownProcess(`Marketplace Metering API is disabled in this region ${this.region}`);
-          break;
-        case 'InternalServiceErrorException':
-          logger.warn({ error }, `${this.getName()} failed unexpectedly.`
-            + 'An internal error has occurred. The request will be retried.'
-            + 'If the problem persists, post a message with details on the AWS forums.');
-          break;
-        case 'InvalidProductCodeException':
-          this.shutdownProcess(`Invalid product code ${this.productCode}`);
-          break;
-        case 'InvalidPublicKeyVersionException':
-          this.shutdownProcess(`Invalid public key version ${this.publicKeyVersion}`);
-          break;
-        case 'PlatformNotSupportedException':
-          this.shutdownProcess('This container is not supported on the underlying platform.'
-            + 'Currently, Amazon ECS, Amazon EKS, and AWS Fargate are supported.');
-          break;
-        case 'AccessDeniedException':
-          this.shutdownProcess('AccessDeniedException because no identity-based policy allows'
-            + ' the aws-marketplace:MeterUsage action');
-          break;
-        default:
-          this.shutdownProcess(`${this.getName()} failed with unknown error`);
-          throw error;
-      }
-    } finally {
-      logger.debug({
-        publicKeyVersion: this.publicKeyVersion,
-        productCode: this.productCode,
-      }, `${this.getName()} finished`);
-    }
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  shutdownProcess(message) {
-    logger.error(`${message}`);
-    process.exit(1);
+    commands.forEach((command) => sendCommandOrFail(client, command, 'aws-marketplace:MeterUsage'))
   }
 
   async activeDeployments(deploymentIds) {
--- src/jobs/meter/AwsRegisterUsage.js
+++ src/jobs/meter/AwsRegisterUsage.js
@@ -1,5 +1,6 @@
 const { MarketplaceMeteringClient, RegisterUsageCommand } = require('@aws-sdk/client-marketplace-metering');
 const uuid = require('uuid').v4;
+const { sendCommandOrFail } = require('./AwsMeterUtils');
 const AbstractJob = require('../AbstractJob');
 const AWS = require('../../utils/aws');
 
@@ -48,62 +49,7 @@ class AwsRegisterUsage extends AbstractJob {
     }
     const command = new RegisterUsageCommand(params);
     logger.trace({ params }, 'created command');
-    try {
-      const data = await client.send(command);
-      logger.debug({ data }, `${this.getName()} RegisterUsageCommand executed successfully`);
-    } catch (error) {
-      logger.error({ error }, `${this.getName()} failed`);
-
-      switch (error.name) {
-        case 'UnrecognizedClientException':
-        case 'CredentialsProviderError':
-        case 'CustomerNotEntitledException':
-          this.shutdownProcess('You are not entitled to use this container in your account');
-          break;
-        case 'DisabledApiException':
-          this.shutdownProcess(`Marketplace Metering API is disabled in this region ${this.region}`);
-          break;
-        case 'InternalServiceErrorException':
-          logger.warn({ error }, `${this.getName()} failed unexpectedly.`
-            + 'An internal error has occurred. The request will be retried.'
-            + 'If the problem persists, post a message with details on the AWS forums.');
-          break;
-        case 'InvalidProductCodeException':
-          this.shutdownProcess(`Invalid product code ${this.productCode}`);
-          break;
-        case 'InvalidPublicKeyVersionException':
-          this.shutdownProcess(`Invalid public key version ${this.publicKeyVersion}`);
-          break;
-        case 'PlatformNotSupportedException':
-          this.shutdownProcess('This container is not supported on the underlying platform.'
-            + 'Currently, Amazon ECS, Amazon EKS, and AWS Fargate are supported.');
-          break;
-        case 'AccessDeniedException':
-          this.shutdownProcess('AccessDeniedException because no identity-based policy allows'
-            + ' the aws-marketplace:RegisterUsage action')
-          break;
-        default:
-          this.shutdownProcess(`${this.getName()} failed with unknown error`);
-          throw error;
-      }
-    } finally {
-      logger.debug({
-        publicKeyVersion: this.publicKeyVersion,
-        productCode: this.productCode,
-      }, `${this.getName()} finished`);
-      client.destroy();
-    }
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  shutdownProcess(message) {
-    logger.error(`${message}`);
-    process.exit(1);
-  }
-
-  validateResponse(response, params) {
-    logger.debug({ response, params }, `${this.getName()} validating response`);
-    return true;
+    sendCommandOrFail(client, command, 'aws-marketplace:RegisterUsage')
   }
 }
 
