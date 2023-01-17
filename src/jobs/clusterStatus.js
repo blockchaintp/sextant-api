67fcc69816b1818e69497b0998234f2d8f1f1c27
Mikeala Sheldt <mikaela@blockchaintp.com>
2023-01-17 10:55:30 -0500

    fix(clusterStatus-job): updated to allow kubectl to be set as a mock during testing
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/jobs/clusterStatus.js
+++ src/jobs/clusterStatus.js
@@ -1,94 +1,77 @@
-// Periodically check on the status of the cluster and update the cluster status in the DB if it has changed.
-// This will likely be a new job that will be run on a schedule.
-
-// clusters are listed in the DB
-// we will need the cluster info/credentials in order to ask kubernetes for the status
-// kubectl describe? kubectl get? kubectl status?
-// we may need to parse the output of the command to get the actual status
-// update the cluster status in the DB if it has changed
-
-// Are we using the kubernetes API or the kubectl command? - API
-
-// attempt to get a list of namespaces from the cluster
-
-// schedule.scheduleJob('*/10 * * * *'
-
+const Scheduler = require('node-schedule')
 const logger = require('../logging').getLogger({
   name: 'jobs/clusterStatus',
 })
+
+const { CLUSTER_STATUS } = require('../config')
 const ClusterKubectl = require('../utils/clusterKubectl')
 
-const getAllClusters = async (store) => {
-  // get a list of all of the clusters in the database
-  const clusters = await store.cluster.list({
-    deleted: false,
-  })
+class ClusterStatus {
+  constructor(store, Kubectl = ClusterKubectl, test = false) {
+    this.store = store
+    this.Kubectl = Kubectl
+    this.test = test
+  }
 
-  logger.debug({
-    fn: 'getAllClusters',
-    clusters,
-  })
-  return clusters
-}
-
-const defineClusterKubectl = async (cluster, store) => {
-  await ClusterKubectl({
-    cluster,
-    store,
-  })
-}
-
-const updateClusterStatus = async (cluster, store) => {
-  // set the cluster as inactive (deleted?)
-  const updatedCluster = await store.cluster.update({
-    id: cluster.id,
-    status: 'inactive',
-  })
-
-  logger.debug({
-    fn: 'updateClusterStatus',
-    updatedCluster: { name: updatedCluster.name, status: updatedCluster.status },
-  })
-  return updatedCluster
-}
-
-const pingClusters = (clusters, store) => {
-  // define a new kubectl instance for each cluster in the list from the DB
-  // ping for namespaces, update statuses, handle errors
-  clusters.forEach(async (cluster) => {
-    const clusterKubectl = await defineClusterKubectl(cluster, store)
-
-    // if there are namespaces, the status is active
-    const namespaces = await clusterKubectl.getNamespaces()
-
-    logger.debug({
-      fn: 'clusterKubectl.getNamespaces',
-      namespaces,
+  async getAllClusters() {
+    // get a list of all of the clusters in the database
+    const clusters = await this.store.cluster.list({
+      deleted: false,
     })
+    logger.debug({
+      fn: 'getAllClusters',
+      clusters,
+    })
+    return clusters
+  }
 
-    if (!namespaces) {
-      // if there are no namespaces, the status is inactive
-      await updateClusterStatus(cluster, store)
+  async updateClusterStatus(cluster, status) {
+    // set the cluster as inactive (deleted?)
+    const updatedCluster = await this.store.cluster.update({
+      id: cluster.id,
+      status,
+    })
+    logger.debug({
+      fn: 'updateClusterStatus',
+      updatedCluster: { name: updatedCluster.name, status: updatedCluster.status },
+    })
+    return updatedCluster
+  }
+
+  async inquire(cluster) {
+    let clusterKubectl
+
+    if (this.test === true) {
+      clusterKubectl = new this.Kubectl(cluster)
+    } else {
+      clusterKubectl = await this.Kubectl({ cluster, store: this.store })
     }
 
-    // TODO handle errors
-  })
+    try {
+      await clusterKubectl.getNamespaces()
+      // if the status is good - update the cluster status to active
+      if (cluster.status === CLUSTER_STATUS.error) {
+        await this.updateClusterStatus(cluster, CLUSTER_STATUS.provisioned)
+      }
+    } catch (e) {
+      // if the status is bad - update the cluster status to inactive
+      if (cluster.status === CLUSTER_STATUS.provisioned) {
+        await this.updateClusterStatus(cluster, CLUSTER_STATUS.error)
+      }
+    }
+  }
+
+  async run() {
+    const clusters = await this.getAllClusters(this.store)
+    await clusters.forEach(this.inquire.bind(this))
+  }
+
+  start() {
+    logger.info('Starting cluster status job')
+    this.job = Scheduler.scheduleJob('cluster_inquiery', '*/10 * * * *', () => {
+      this.run()
+    })
+  }
 }
 
-const clusterStatus = async (store) => {
-  const clusters = await getAllClusters(store)
-
-  pingClusters(clusters, store)
-}
-
-module.exports = clusterStatus
-
-/*
-
-  ***** Questions *****
-  - What is the best way to handle errors with promises?
-  - Does pingClusters need to be async?
-  - How are cluster statuses defined?
-  - Is there a reason to include deleted clusters in the list of clusters?
-
-*/
+module.exports = { ClusterStatus }
