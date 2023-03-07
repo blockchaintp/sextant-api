f0df6b2b9f224ebd576fea9eb5583700acb2753a
Kevin O'Donnell <kevin.odonnell@btp.works>
2023-03-06 21:43:46 -0500

    refactor(store): convert clustersecretstore to ts
    Signed-off-by: Kevin O'Donnell <kevin.odonnell@btp.works>

--- test/store/clustersecret.test.js
+++ test/store/clustersecret.test.js
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 'use strict'
 
 const FileSecretStoreTest = require('./file_secret_store_test')
-const ClusterSecretStore = require('../../src/store/clustersecret')
+const { ClusterSecretStore } = require('../../src/store/clustersecret')
 
 FileSecretStoreTest({
   GetStore: ClusterSecretStore,
