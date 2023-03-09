8a82c7d783cf4996ebbfb3c03b1b739dcbc30920
Kevin O'Donnell <kevin.odonnell@btp.works>
2023-03-08 20:07:33 -0500

    refactor: convert damlRPC and deploymentPodProxy to ts
    Signed-off-by: Kevin O'Donnell <kevin.odonnell@btp.works>

---
+++ src/api/damlRPC.ts
@@ -0,0 +1,485 @@
+/* eslint-disable camelcase */
+/*
+ * Copyright © 2020 Blockchain Technology Partners Limited All Rights Reserved
+ *
+ * License: Product
+ */
+import * as bluebird from 'bluebird'
+import * as fs from 'fs'
+import * as jwt from 'jsonwebtoken'
+import * as memoize from 'memoizee'
+import { getLogger } from '../logging'
+import { Store } from '../store'
+import { Deployment } from '../store/model/model-types'
+import { DatabaseIdentifier } from '../store/model/scalar-types'
+import * as deploymentNames from '../utils/deploymentNames'
+import { DeploymentPodProxy } from '../utils/deploymentPodProxy'
+import * as Grpcurl from '../utils/grpcurl'
+import { SecretLoader } from '../utils/secretLoader'
+import * as database from './database'
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const logger = getLogger({
+  name: 'api/damlRPC',
+})
+
+const DAML_GRPC_METHOD_PREFIX = 'com.daml.ledger.api.v1.'
+const DAML_RPC_PORT = 39000
+
+export type DamlJWTClaim = {
+  actAs?: string[]
+  admin?: boolean
+  applicationId?: string
+  ledgerId?: string
+  public?: boolean
+  readAs?: string[]
+}
+
+const getAdminJWTToken = ({ id, store }: { id: DatabaseIdentifier; store: Store }) =>
+  getJWTToken({
+    id,
+    payload: {
+      admin: true,
+      public: true,
+    },
+    store,
+  })
+
+const getJWTToken = async ({ id, payload, store }: { id: DatabaseIdentifier; payload: DamlJWTClaim; store: Store }) => {
+  const privateKey = await getPrivateKey({
+    id,
+    store,
+  })
+  return new bluebird((resolve, reject) => {
+    jwt.sign(
+      {
+        'https://daml.com/ledger-api': payload,
+        // eslint-disable-next-line consistent-return
+      },
+      privateKey,
+      {
+        algorithm: 'RS256',
+      },
+      (err, result) => {
+        if (err) return reject(err)
+        return resolve(result)
+      }
+    )
+  })
+}
+
+const getPrivateKey = async ({ id, store }: { id: DatabaseIdentifier; store: Store }) => {
+  const deployment = await store.deployment.get({
+    id,
+  })
+
+  const secretLoader = new SecretLoader({
+    store,
+    id,
+  })
+
+  const secretName = getCertSecretName(deployment)
+  const secret = await secretLoader.getSecret(secretName)
+  if (!secret || !secret.data) throw new Error(`no secret found to sign token ${secretName}`)
+  const keyBase64 = secret.data['jwt.key']
+  if (!keyBase64) throw new Error(`no value found to sign token ${secretName} -> jwt.key`)
+  return Buffer.from(keyBase64, 'base64').toString('utf8')
+}
+
+const getCertSecretName = (deployment: Deployment) => {
+  const fullName = deploymentNames.deploymentReleaseFullName(deployment)
+  return `${fullName}-cert`
+}
+
+async function callProxy({
+  id,
+  store,
+  handler,
+}: {
+  handler: (args: { port: number }) => Promise<unknown>
+  id: DatabaseIdentifier
+  store: Store
+}) {
+  const proxy = await DeploymentPodProxy({
+    store,
+    id,
+    labelPattern: 'app.kubernetes.io/instance=<name>,component=daml',
+  })
+
+  const pods = await proxy.getPods()
+
+  if (pods.length <= 0) throw new Error('The daml-rpc pod cannot be found.')
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  logger.debug({ fn: 'getLedgerId', numberOfPods: pods.length }, 'found daml pods')
+
+  return proxy.request({
+    pod: pods[0].metadata.name,
+    port: DAML_RPC_PORT,
+    handler,
+  })
+}
+
+const m_getLedgerId = ({ id, store }: { id: DatabaseIdentifier; store: Store }) => {
+  const handler = async ({ port }: { port: number }) => {
+    const token = await getAdminJWTToken({
+      id,
+      store,
+    })
+    const grpccurl = Grpcurl({
+      token,
+      port,
+      prefix: DAML_GRPC_METHOD_PREFIX,
+    })
+    const { ledgerId } = (await grpccurl({
+      service: 'LedgerIdentityService',
+      method: 'GetLedgerIdentity',
+      data: undefined,
+    })) as { ledgerId: string }
+    return ledgerId
+  }
+
+  return callProxy({ id, store, handler }) as Promise<string>
+}
+const getLedgerId = memoize(m_getLedgerId, { maxAge: 30000, promise: true })
+
+const m_getParticipantId = ({ id, store }: { id: DatabaseIdentifier; store: Store }) => {
+  const handler = async ({ port }: { port: number }) => {
+    const token = await getAdminJWTToken({
+      id,
+      store,
+    })
+    const grpccurl = Grpcurl({
+      token,
+      port,
+      prefix: DAML_GRPC_METHOD_PREFIX,
+    })
+    const { participantId } = (await grpccurl({
+      service: 'admin.PartyManagementService',
+      method: 'GetParticipantId',
+      data: undefined,
+    })) as { participantId: { participantId: string } }
+    return participantId
+  }
+
+  return callProxy({ id, store, handler }) as Promise<string>
+}
+const getParticipantId = memoize(m_getParticipantId, { maxAge: 30000, promise: true })
+
+export class DamlRPC {
+  private store: Store
+  constructor({ store }: { store: Store }) {
+    if (!store) {
+      throw new Error('Daml rpc requires a store')
+    }
+    this.store = store
+  }
+
+  public async addParty({
+    id,
+    partyName,
+    partyIdHint,
+  }: {
+    id: DatabaseIdentifier
+    partyIdHint: string
+    partyName: string
+  }) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    logger.info({
+      fn: 'addParty',
+      id,
+      partyName,
+      partyIdHint,
+    })
+
+    const proxy = await DeploymentPodProxy({
+      store: this.store,
+      id,
+      labelPattern: 'app.kubernetes.io/instance=<name>,component=daml',
+    })
+
+    const pods = await proxy.getPods()
+
+    if (pods.length <= 0) throw new Error('The daml-rpc pod cannot be found.')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    logger.debug({ fn: 'addParty', numberOfPods: pods.length }, 'found daml pods')
+
+    const pod = pods[0]
+
+    const result = await proxy.request({
+      pod: pod.metadata.name,
+      port: DAML_RPC_PORT,
+      handler: async ({
+        port,
+        // eslint-disable-next-line consistent-return
+      }) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        logger.debug(`Allocating party to ${pod.metadata.name}`)
+        const data = {
+          partyIdHint: partyIdHint || partyName,
+          displayName: partyName,
+        }
+
+        const token = await getAdminJWTToken({
+          id,
+          store: this.store,
+        })
+
+        const grpccurl = Grpcurl({
+          token,
+          port,
+          prefix: DAML_GRPC_METHOD_PREFIX,
+        })
+
+        const { partyDetails } = (await grpccurl({
+          service: 'admin.PartyManagementService',
+          method: 'AllocateParty',
+          data,
+        })) as { partyDetails: { displayName: string }[] }
+
+        return partyDetails
+      },
+    })
+    // eslint-disable-next-line no-unneeded-ternary
+    return result ? true : false
+  }
+
+  public async generateAdminToken({ id, applicationId }: { applicationId: string; id: DatabaseIdentifier }) {
+    if (!applicationId) throw new Error('applicationId must be given to api.damlRPC.generateAdminToken')
+
+    const ledgerId = await getLedgerId({
+      id,
+      store: this.store,
+    })
+
+    return getJWTToken({
+      id,
+      payload: {
+        public: true,
+        admin: true,
+        ledgerId,
+        applicationId,
+      },
+      store: this.store,
+    })
+  }
+
+  public async generatePartyToken({
+    id,
+    applicationId,
+    readAs,
+    actAs,
+  }: {
+    actAs: string[]
+    applicationId: string
+    id: DatabaseIdentifier
+    readAs: string[]
+  }) {
+    if (!applicationId) throw new Error('applicationId must be given to api.damlRPC.generatePartyTokens')
+    if (!readAs) throw new Error('readAs must be given to api.damlRPC.generatePartyTokens')
+    if (!actAs) throw new Error('actAs must be given to api.damlRPC.generatePartyTokens')
+
+    const ledgerId = await getLedgerId({
+      id,
+      store: this.store,
+    })
+
+    return getJWTToken({
+      id,
+      payload: {
+        public: true,
+        ledgerId,
+        applicationId,
+        readAs,
+        actAs,
+      },
+      store: this.store,
+    })
+  }
+
+  public async getArchives({ id }: { id: DatabaseIdentifier }) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    logger.info({
+      fn: 'getArchives',
+      id,
+    })
+
+    // This is responsible for port forwarding
+    const proxy = await DeploymentPodProxy({
+      store: this.store,
+      id,
+      labelPattern: 'app.kubernetes.io/instance=<name>,component=daml',
+    })
+
+    // We need to get all pods here
+    const pods = await proxy.getPods()
+
+    if (pods.length <= 0) throw new Error('The daml-rpc pod cannot be found.')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    logger.debug({ fn: 'getArchives', numberOfPods: pods.length }, 'found daml pods')
+
+    const pod = pods[0]
+
+    // Extract archive information from one pod only
+    // This is regardless of all validator pods
+    // reaching consensus
+    try {
+      return await bluebird.resolve(
+        proxy.request({
+          pod: pod.metadata.name,
+          port: DAML_RPC_PORT,
+          handler: async ({ port }) => {
+            const token = await bluebird.resolve(
+              getAdminJWTToken({
+                id,
+                store: this.store,
+              })
+            )
+
+            const grpccurl = Grpcurl({
+              token,
+              port,
+              prefix: DAML_GRPC_METHOD_PREFIX,
+            })
+
+            const response = (await grpccurl({
+              service: 'admin.PackageManagementService',
+              method: 'ListKnownPackages',
+              data: undefined,
+            })) as { packageDetails: unknown[] }
+            const packages = response.packageDetails
+
+            return packages ? packages.sort() : []
+          },
+        })
+      )
+    } catch (error: unknown) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      logger.error({
+        fn: 'getArchives',
+        error,
+      })
+      throw error
+    }
+  }
+
+  public getTimeServiceInfo() {
+    return database.damlTimeService
+  }
+
+  public async registerParticipant({ id, publicKey }: { id: DatabaseIdentifier; publicKey: string }) {
+    if (!publicKey) throw new Error('publicKey must be given to api.damlRPC.registerParticipant')
+
+    const participantId = await getParticipantId({
+      id,
+      store: this.store,
+    })
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    logger.info({
+      fn: 'registerParticipant',
+      participantId,
+      publicKey,
+    })
+
+    database.damlParticipants.push({
+      damlId: database.getKey(),
+      participantId,
+      publicKey,
+      parties: [],
+    })
+
+    return database.damlParticipants
+  }
+
+  public updateKey({ oldPublicKey, newPublicKey }: { oldPublicKey: string; newPublicKey: string }) {
+    if (!oldPublicKey) throw new Error('oldPublicKey must be given to api.damlRPC.updateKey')
+    if (!newPublicKey) throw new Error('newPublicKey must be given to api.damlRPC.updateKey')
+    const participant = database.damlParticipants.find((oneParticipant) => oneParticipant.publicKey === oldPublicKey)
+    if (!participant) throw new Error(`no participant found with publicKey ${oldPublicKey}`)
+    participant.publicKey = newPublicKey
+    return true
+  }
+
+  public async uploadArchive({
+    id,
+    name,
+    size,
+    localFilepath,
+  }: {
+    id: DatabaseIdentifier
+    localFilepath: string
+    name: string
+    size: number
+  }) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    logger.info({
+      fn: 'uploadArchive',
+      id,
+      name,
+      size,
+      localFilepath,
+    })
+
+    const content = fs.readFileSync(localFilepath)
+    const contentBase64 = content.toString('base64')
+
+    // This is responsible for port forwarding
+    const proxy = await DeploymentPodProxy({
+      store: this.store,
+      id,
+      labelPattern: 'app.kubernetes.io/instance=<name>,component=daml',
+    })
+
+    // We need to get all pods here
+    const pods = await proxy.getPods()
+
+    if (pods.length <= 0) throw new Error('The daml-rpc pod cannot be found.')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    logger.debug({ fn: 'uploadArchive', numberOfPods: pods.length }, 'found daml pods')
+
+    const pod = pods[0]
+
+    return proxy.request({
+      pod: pod.metadata.name,
+      port: DAML_RPC_PORT,
+      handler: async ({ port }) => {
+        const token = await getAdminJWTToken({
+          id,
+          store: this.store,
+        })
+
+        const grpccurl = Grpcurl({
+          token,
+          port,
+          prefix: DAML_GRPC_METHOD_PREFIX,
+        })
+
+        const data = {
+          dar_file: contentBase64,
+        }
+
+        try {
+          await grpccurl({
+            service: 'admin.PackageManagementService',
+            method: 'UploadDarFile',
+            data,
+          })
+        } catch (e) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+          if (e.toString().indexOf('Invalid DAR') >= 0) {
+            throw new Error("that file doesn't look like a DAR file")
+          } else {
+            throw e
+          }
+        }
+
+        return grpccurl({
+          service: 'admin.PackageManagementService',
+          method: 'ListKnownPackages',
+          data: undefined,
+        })
+      },
+    })
+  }
+}
+
+module.exports = DamlRPC
--- src/controller/daml.js
+++ src/controller/daml.js
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const KeyPair = require('../utils/sextantKeyPair')
 const KeyManager = require('../api/keyManager')
 const DamlRPC = require('../api/damlRPC')
@@ -7,14 +12,12 @@ const DamlController = ({ store }) => {
   const keyManager = KeyManager({
     store,
   })
-  const damlRPC = DamlRPC({
+  const damlRPC = new DamlRPC({
     store,
   })
   const settingsTP = SettingsTP()
 
-  const getKeyManagerKeys = async ({
-    id,
-  }) => {
+  const getKeyManagerKeys = async ({ id }) => {
     const keyPair = await KeyPair.get({
       store,
       deployment: id,
@@ -26,24 +29,19 @@ const DamlController = ({ store }) => {
     })
   }
 
-  const getEnrolledKeys = async () => settingsTP.getEnrolledKeys()
+  const getEnrolledKeys = () => settingsTP.getEnrolledKeys()
 
-  const addEnrolledKey = async ({
-    publicKey,
-  }) => settingsTP.addEnrolledKey({
-    publicKey,
-  })
+  const addEnrolledKey = ({ publicKey }) =>
+    settingsTP.addEnrolledKey({
+      publicKey,
+    })
 
-  const getParticipants = async ({
-    id,
-  }) => damlRPC.getParticipants({
-    id,
-  })
+  const getParticipants = ({ id }) =>
+    damlRPC.getParticipants({
+      id,
+    })
 
-  const registerParticipant = async ({
-    id,
-    publicKey,
-  }) => {
+  const registerParticipant = ({ id, publicKey }) => {
     if (!id) throw new Error('id must be given to controller.deployment.registerParticipant')
     if (!publicKey) throw new Error('publicKey must be given to controller.deployment.registerParticipant')
 
@@ -53,14 +51,11 @@ const DamlController = ({ store }) => {
     })
   }
 
-  const rotateParticipantKey = async ({
-    id,
-    publicKey,
-  }) => {
+  const rotateParticipantKey = async ({ id, publicKey }) => {
     if (!id) throw new Error('id must be given to controller.deployment.rotateParticipantKey')
     if (!publicKey) throw new Error('publicKey must be given to controller.deployment.rotateParticipantKey')
 
-    const newKey = await keyManager.rotateRPCKey({
+    const newKey = keyManager.rotateRPCKey({
       publicKey,
     })
 
@@ -72,11 +67,7 @@ const DamlController = ({ store }) => {
     return true
   }
 
-  const addParty = async ({
-    id,
-    partyName,
-    partyIdHint,
-  }) => {
+  const addParty = async ({ id, partyName, partyIdHint }) => {
     if (!id) throw new Error('id must be given to controller.deployment.addParty')
     if (!partyName) throw new Error('partyName must be given to controller.deployment.addParty')
 
@@ -89,12 +80,7 @@ const DamlController = ({ store }) => {
     return true
   }
 
-  const generatePartyToken = async ({
-    id,
-    applicationId,
-    readAs,
-    actAs,
-  }) => {
+  const generatePartyToken = async ({ id, applicationId, readAs, actAs }) => {
     if (!id) throw new Error('id must be given to controller.deployment.generatePartyToken')
     if (!applicationId) throw new Error('applicationId must be given to controller.deployment.generatePartyToken')
     if (!readAs) throw new Error('readAs must be given to controller.deployment.generatePartyToken')
@@ -112,10 +98,7 @@ const DamlController = ({ store }) => {
     }
   }
 
-  const generateAdminToken = async ({
-    id,
-    applicationId,
-  }) => {
+  const generateAdminToken = async ({ id, applicationId }) => {
     if (!id) throw new Error('id must be given to controller.deployment.generatePartyToken')
     if (!applicationId) throw new Error('applicationId must be given to controller.deployment.generatePartyToken')
 
@@ -129,18 +112,12 @@ const DamlController = ({ store }) => {
     }
   }
 
-  const getArchives = async ({
-    id,
-  }) => damlRPC.getArchives({
-    id,
-  })
+  const getArchives = ({ id }) =>
+    damlRPC.getArchives({
+      id,
+    })
 
-  const uploadArchive = async ({
-    id,
-    name,
-    size,
-    localFilepath,
-  }) => {
+  const uploadArchive = async ({ id, name, size, localFilepath }) => {
     if (!id) throw new Error('id must be given to controller.deployment.uploadArchive')
     if (!name) throw new Error('name must be given to controller.deployment.uploadArchive')
     if (!size) throw new Error('size must be given to controller.deployment.uploadArchive')
@@ -156,7 +133,7 @@ const DamlController = ({ store }) => {
     return data
   }
 
-  const getTimeServiceInfo = async () => damlRPC.getTimeServiceInfo()
+  const getTimeServiceInfo = () => damlRPC.getTimeServiceInfo()
 
   return {
     getKeyManagerKeys,
