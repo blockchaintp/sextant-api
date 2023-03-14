7ae1acc8f7e4c79f91e7e84914300e0d4371d7a7
Kevin O'Donnell <kevin.odonnell@btp.works>
2023-03-14 00:45:19 -0400

    refactor: eliminate http module
    Signed-off-by: Kevin O'Donnell <kevin.odonnell@btp.works>

--- src/router/taekion.js
+++ src/router/taekion.js
@@ -1,81 +1,64 @@
-const httpUtils = require('../utils/http')
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+function badRequest(res, error = 'The request was bad', code = 400) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  res.status(code).json({
+    error,
+  })
+}
 
 const TaekionRoutes = (controllers) => {
-
   const listKeys = async (req, res, next) => {
-    const {
-      id,
-    } = req.params
+    const { id } = req.params
     const data = await controllers.taekion.listKeys({
       deployment: id,
     })
-    res
-      .status(200)
-      .json(data)
+    res.status(200).json(data)
   }
 
   const createKey = async (req, res, next) => {
-    const {
-      id,
-    } = req.params
-    const {
-      name,
-    } = req.body
+    const { id } = req.params
+    const { name } = req.body
 
-    if(!name) return httpUtils.badRequest(res, `name required`)
-    
+    if (!name) return badRequest(res, `name required`)
+
     const data = await controllers.taekion.createKey({
       deployment: id,
       name,
     })
-    res
-      .status(201)
-      .json(data)
+    res.status(201).json(data)
   }
 
   const deleteKey = async (req, res, next) => {
-    const {
-      id,
-      keyId,
-    } = req.params
-    
+    const { id, keyId } = req.params
+
     const data = await controllers.taekion.deleteKey({
       deployment: id,
       id: keyId,
     })
-    res
-      .status(201)
-      .json(data)
+    res.status(201).json(data)
   }
 
   const listVolumes = async (req, res, next) => {
-    const {
-      id,
-    } = req.params
+    const { id } = req.params
     const data = await controllers.taekion.listVolumes({
       deployment: id,
     })
-    res
-      .status(200)
-      .json(data)
+    res.status(200).json(data)
   }
 
   const createVolume = async (req, res, next) => {
-    const {
-      id,
-    } = req.params
-    const {
-      name,
-      compression,
-      encryption,
-      fingerprint,
-    } = req.body
+    const { id } = req.params
+    const { name, compression, encryption, fingerprint } = req.body
 
-    if(!name) return httpUtils.badRequest(res, `name required`)
-    if(!compression) return httpUtils.badRequest(res, `compression required`)
-    if(!encryption) return httpUtils.badRequest(res, `encryption required`)
-    if(encryption != 'NONE' && !fingerprint) return httpUtils.badRequest(res, `fingerprint required`)
-    
+    if (!name) return badRequest(res, `name required`)
+    if (!compression) return badRequest(res, `compression required`)
+    if (!encryption) return badRequest(res, `encryption required`)
+    if (encryption != 'NONE' && !fingerprint) return badRequest(res, `fingerprint required`)
+
     const data = await controllers.taekion.createVolume({
       deployment: id,
       name,
@@ -83,131 +66,83 @@ const TaekionRoutes = (controllers) => {
       encryption,
       fingerprint,
     })
-    res
-      .status(201)
-      .json(data)
+    res.status(201).json(data)
   }
 
   const updateVolume = async (req, res, next) => {
-    const {
-      id,
-      volume,
-    } = req.params
-    const {
-      name,
-    } = req.body
+    const { id, volume } = req.params
+    const { name } = req.body
 
     const data = await controllers.taekion.updateVolume({
       deployment: id,
       volume,
       name,
     })
-    res
-      .status(200)
-      .json(data)
+    res.status(200).json(data)
   }
 
   const deleteVolume = async (req, res, next) => {
-    const {
-      id,
-      volume,
-    } = req.params
-    
+    const { id, volume } = req.params
+
     const data = await controllers.taekion.deleteVolume({
       deployment: id,
       volume,
     })
-    res
-      .status(200)
-      .json(data)
+    res.status(200).json(data)
   }
 
   const listSnapshots = async (req, res, next) => {
-    const {
-      id,
-      volume,
-    } = req.params
+    const { id, volume } = req.params
     const data = await controllers.taekion.listSnapshots({
       deployment: id,
       volume,
     })
-    res
-      .status(200)
-      .json(data)
+    res.status(200).json(data)
   }
 
   const createSnapshot = async (req, res, next) => {
-    const {
-      id,
-      volume,
-    } = req.params
-    const {
-      name,
-    } = req.body
+    const { id, volume } = req.params
+    const { name } = req.body
 
-    if(!name) return httpUtils.badRequest(res, `name required`)
+    if (!name) return badRequest(res, `name required`)
 
     const data = await controllers.taekion.createSnapshot({
       deployment: id,
       volume,
       name,
     })
-    res
-      .status(201)
-      .json(data)
+    res.status(201).json(data)
   }
 
   const deleteSnapshot = async (req, res, next) => {
-    const {
-      id,
-      volume,
-      snapshotName,
-    } = req.params
-    
+    const { id, volume, snapshotName } = req.params
+
     const data = await controllers.taekion.deleteSnapshot({
       deployment: id,
       volume,
       snapshotName,
     })
-    res
-      .status(200)
-      .json(data)
+    res.status(200).json(data)
   }
 
   const explorerListDirectory = async (req, res, next) => {
-    const {
-      id,
-      volume,
-      inode,
-    } = req.params
+    const { id, volume, inode } = req.params
 
-    const {
-      snapshot,
-    } = req.query
-    
+    const { snapshot } = req.query
+
     const data = await controllers.taekion.explorerListDirectory({
       deployment: id,
       volume,
       inode,
       snapshot,
     })
-    res
-      .status(200)
-      .json(data)
+    res.status(200).json(data)
   }
 
   const explorerDownloadFile = async (req, res, next) => {
-    const {
-      id,
-      volume,
-      directory_inode,
-      file_inode,
-    } = req.params
+    const { id, volume, directory_inode, file_inode } = req.params
 
-    const {
-      download_filename,
-      snapshot,
-    } = req.query
+    const { download_filename, snapshot } = req.query
 
     await controllers.taekion.explorerDownloadFile({
       deployment: id,
@@ -220,11 +155,12 @@ const TaekionRoutes = (controllers) => {
     })
   }
 
-  const restApiProxy = (req, res, next) => controllers.taekion.restApiProxy({
-    deployment: req.params.id,
-    req,
-    res,
-  })
+  const restApiProxy = (req, res, next) =>
+    controllers.taekion.restApiProxy({
+      deployment: req.params.id,
+      req,
+      res,
+    })
 
   return {
     listKeys,
--- src/utils/http.ts
+++
@@ -1,6 +0,0 @@
-export function badRequest(res: any, error = 'The request was bad', code = 400) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-  res.status(code).json({
-    error,
-  })
-}
